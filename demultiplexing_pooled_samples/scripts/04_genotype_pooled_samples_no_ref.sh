7292c66e228c909c74a67da9d3f0f6db9b133dd7
emma2207 <emma.lathouwers@cuanschutz.edu>
2026-04-04 08:06:07 -0600

    debugging

--- demultiplexing_pooled_samples/scripts/04_genotype_pooled_samples_no_ref.sh
+++ demultiplexing_pooled_samples/scripts/04_genotype_pooled_samples_no_ref.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-#SBATCH --array=1-10
 #SBATCH --nodes=1
-#SBATCH --qos=long
+#SBATCH --qos=normal
 #SBATCH --partition=amilan
-#SBATCH --mem=48G
-#SBATCH --time=3-00:00:00
-#SBATCH --ntasks=20
+#SBATCH --mem=240G
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=4
 #SBATCH --account=amc-general
 #SBATCH --job-name=cellsnp-lite
 #SBATCH --output=03_genotype_pooled_%A_%a.log
@@ -17,20 +16,36 @@
 module load anaconda
 conda activate cellsnp-lite_install
 
-export pool=$SLURM_ARRAY_TASK_ID
+dataset="hgsoc-new"
+pool=10
 
-# Original location is /scratch/alpine/$USER/hgsoc/pooled
-mkdir -p cellSNP_no_ref/pool${pool}
-
+# Original location is /scratch/alpine/$USER/hgsoc/demultiplexing
 original_location=$(pwd)
-pooled_bam_location="$original_location/bam/pool${pool}"
-output_location="$original_location/cellSNP_no_ref/pool${pool}"
+pooled_bam_location="$original_location/results/star/${dataset}/pool${pool}"
+output_location="$original_location/results/cellSNP/${dataset}/pool${pool}"
+barcodes="${original_location}/barcodes_pool${pool}.tsv"
+
+if [ ! -f "$barcodes" ]; then
+    echo "Barcode file not found: $barcodes"
+    exit 1
+fi
+
+mkdir -p ${output_location}
+
+# Remove -1 suffix from barcodes if present (e.g. from 10x v3 chemistry)
+if cat $barcodes | head -n 1 | grep -qE -- '-1$'; then
+    echo "Removing -1 suffix from barcodes in $barcodes"
+    cat $barcodes | sed 's/-1$//' > $output_location/barcodes.tsv
+    barcodes="$output_location/barcodes.tsv"
+else
+    echo "No -1 suffix found in barcodes, using original file: $barcodes"
+fi
 
 cellsnp-lite \
-	-s $pooled_bam_location/pooled.bam \
-	-b barcodes/barcodes_pool${pool}.tsv \
-	-O $output_location \
-	-p 20 \
+	-s "$pooled_bam_location/Aligned.sortedByCoord.out.bam" \
+	-b "$barcodes" \
+	-O "$output_location" \
+	-p 4 \
 	--minMAF=0.1 \
-	--minCOUNT=100 \
+	--minCOUNT=20 \
 	--gzip
