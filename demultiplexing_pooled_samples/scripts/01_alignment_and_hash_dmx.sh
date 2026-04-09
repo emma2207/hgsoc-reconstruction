716d41ef6178f2dcf8e54e292941677e8c3eea5f
emma2207 <emma.lathouwers@cuanschutz.edu>
2026-04-09 08:29:23 -0600

    fix?

--- demultiplexing_pooled_samples/scripts/01_alignment_and_hash_dmx.sh
+++ demultiplexing_pooled_samples/scripts/01_alignment_and_hash_dmx.sh
@@ -1,25 +1,35 @@
 #!/bin/sh
 
-#SBATCH --array=1-10
+#SBATCH --array=1
 #SBATCH --nodes=1
 #SBATCH --qos=long
 #SBATCH --partition=amilan
 #SBATCH --mem=64G
-#SBATCH --time=5-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --ntasks=16
 #SBATCH --account=amc-general
-#SBATCH --job-name=cellranger_multi
-#SBATCH --output=01_cellranger_multi_%A_%a.log
-#SBATCH --error=01_cellranger_multi_%A_%a.err
+#SBATCH --job-name=cellranger_count
+#SBATCH --output=01_cellranger_count_%A_%a.log
+#SBATCH --error=01_cellranger_count_%A_%a.err
 #SBATCH --mail-user=emma.lathouwers@cuanschutz.edu
 #SBATCH --mail-type=ALL
 
-module use --append /projects/$USER/lmod-files
-module load cellranger/9.0.1
+# module use --append /projects/$USER/lmod-files
+# module load cellranger/9.0.1
+module load cellranger/7.1.0
 
 export pool=$SLURM_ARRAY_TASK_ID
+dataset="hgsoc"
+datatype="pooled_sc"
 
-# Location is /scratch/alpine/$USER/hgsoc/pooled/cellranger
+# Location is /scratch/alpine/$USER/hgsoc/demultiplexing
 
-cellranger multi --id=pool$pool \
-	--csv=cellranger_input/cellranger_multi_input_pool$pool.csv
+
+
+cellranger count \
+	--id=pool$pool \
+	--transcriptome="/projects/$USER/hgsoc/refdata-gex-GRCh38-2024-A" \
+	--fastqs="/pl/active/cgreene-sc-hgsoc/ariel_sc_HGSOC/$datatype/fastq" \
+	--sample=Pool$pool-GEX \
+	--nosecondary \
+	--output-dir="/projects/$USER/test"
